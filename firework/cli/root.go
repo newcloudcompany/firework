3a1e107cebfc2e47d79cb709a89473be6147db73
jlkiri <mail@kirillvasiltsov.com>
2023-07-01 02:57:38 +0900

    tidy up

--- firework/cli/root.go
+++ firework/cli/root.go
@@ -6,19 +6,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "firework",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A tool for launching local Firecracker VM clusters",
+	Long:  `A tool for launching local Firecracker VM clusters`,
 }
 
 func Execute() {
@@ -28,7 +19,3 @@ func Execute() {
 		os.Exit(1)
 	}
 }
-
-// func init() {
-// 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.firework.yaml)")
-// }
