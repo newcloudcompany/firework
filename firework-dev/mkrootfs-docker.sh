e828af192b5cd14411e610e22011f3a1ce547cd1
jlkiri <mail@kirillvasiltsov.com>
2023-07-08 16:16:14 +0900

    make squashfs

--- firework-dev/mkrootfs-docker.sh
+++ firework-dev/mkrootfs-docker.sh
@@ -5,10 +5,26 @@ set -euo pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $script_dir
 
+squashfs_img="rootfs.squashfs"
+squashfs_target_dir="debian-bookworm-rootfs-squashfs"
 rootfs_archive_path="debian-bookworm-rootfs.tar.gz"
+
+function cleanup {
+    echo "Cleanup..."
+    rm -rf "$squashfs_target_dir"
+}
+
+trap cleanup EXIT
 
 buildctl build --frontend=dockerfile.v0 \
     --local context=docker \
     --local dockerfile=docker \
-    --output type=tar,dest=rootfs.tar \
+    --output type=local,dest=debian-bookworm-rootfs-squashfs \
     --opt build-arg:ROOTFS_ARCHIVE_PATH="$rootfs_archive_path"
+
+function mkroot_squashfs {
+    echo "Creating squashfs image of the debian rootfs..."
+    mksquashfs "$squashfs_target_dir" "$squashfs_img" -noappend
+}
+
+mkroot_squashfs
