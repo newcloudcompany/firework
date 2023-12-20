de94091cc092c7862f77ea4aa71e20c6ff03d936
jlkiri <mail@kirillvasiltsov.com>
2023-12-21 03:17:14 +0900

    init

--- init/src/main.rs
+++ init/src/main.rs
@@ -1,21 +1,23 @@
 use std::{
-    env, io,
+    borrow::Borrow,
+    env, io, mem,
     process::{Command, Stdio},
-    sync::mpsc::{self, channel},
-    thread::{self, sleep},
-    time::Duration,
+    sync::mpsc::{self},
+    thread::{self},
 };
 
 use nix::{
     libc::{c_int, pid_t},
     sys::{
         signal::Signal,
-        wait::{waitpid, WaitPidFlag, WaitStatus},
+        wait::{waitpid, WaitStatus},
     },
     unistd::Pid,
 };
 
 use log::{debug, error};
+use nix::sys::signal::kill;
+use signal_hook::consts::signal::SIGINT;
 
 pub fn log_init() {
     // default to "debug" level, just for this bin
@@ -26,49 +28,6 @@ pub fn log_init() {
         .write_style(env_logger::WriteStyle::Never)
         .format_module_path(false)
         .init();
-}
-
-fn main() -> Result<(), anyhow::Error> {
-    // let exe = env::args().nth(1).unwrap_or_else(|| {
-    //     eprintln!("Please provide a path to an executable");
-    //     std::process::exit(1);
-    // });
-    log_init();
-
-    let mut cmd = Command::new("sleep");
-    cmd.arg("10");
-    cmd.stdout(io::stdout())
-        .stderr(io::stderr())
-        .stdin(Stdio::inherit());
-
-    let child = cmd.spawn()?;
-    debug!("Spawned child (pid: {})", child.id());
-    use signal_hook::consts::signal::SIGINT;
-
-    let signals = notify(&[SIGINT])?;
-
-    use nix::sys::signal::kill;
-    let child_pid = child.id() as pid_t;
-
-    thread::spawn(move || {
-        for sig in signals {
-            let signal = Signal::try_from(sig).expect("bad signal");
-            debug!("Received signal: {signal}");
-            match kill(Pid::from_raw(child_pid), signal) {
-                Ok(_) => {
-                    debug!("Sent signal {signal} to the child");
-                    break;
-                }
-                Err(e) => {
-                    error!("Error: {}", e);
-                }
-            }
-        }
-    });
-
-    let exit_code = reap_zombies(child_pid);
-
-    std::process::exit(exit_code);
 }
 
 fn notify(signals: &[c_int]) -> anyhow::Result<mpsc::Receiver<c_int>> {
@@ -85,27 +44,29 @@ fn notify(signals: &[c_int]) -> anyhow::Result<mpsc::Receiver<c_int>> {
 }
 
 fn reap_zombies(child_pid: pid_t) -> i32 {
+    debug!("Starting loop that reaps zombies");
+
     loop {
         match waitpid(Pid::from_raw(-1), None) {
             Ok(status) => match status {
                 WaitStatus::Exited(pid, code) => {
                     if pid.as_raw() == child_pid {
-                        debug!("Main child (pid {pid}) exited with code {code}");
+                        debug!("Main child (pid: {pid}) exited with code {code}");
                         return code;
                     } else {
-                        debug!("Reaped child (pid {pid}). Exit code: {code}");
+                        debug!("Reaped child (pid: {pid}). Exit code: {code}");
                     }
                 }
                 WaitStatus::Signaled(pid, signal, _) => {
                     if pid.as_raw() == child_pid {
-                        debug!("Main child exited with signal (with signal '{signal:?}')");
+                        debug!("Main child exited with signal (with signal: {signal:?})");
                         return 128 + (signal as i32);
                     } else {
                         debug!("Reaped child process (pid: {pid}, signal: {signal:?})")
                     }
                 }
-                _ => {
-                    debug!("Child still running");
+                status => {
+                    debug!("waitpid returned: {status:?}");
                 }
             },
             Err(e) => {
@@ -114,4 +75,46 @@ fn reap_zombies(child_pid: pid_t) -> i32 {
             }
         }
     }
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    log_init();
+
+    let exe = env::args().nth(1).unwrap_or_else(|| {
+        error!("Provide a path to an executable as an argument");
+        log::logger().flush();
+        std::process::exit(1);
+    });
+
+    let mut cmd = Command::new(exe);
+    cmd.stdout(io::stdout())
+        .stderr(io::stderr())
+        .stdin(Stdio::inherit());
+
+    let child = cmd.spawn()?;
+    let child_pid = child.id() as pid_t;
+    debug!("Spawned child, pid: {}", child.id());
+
+    let signals = notify(&[SIGINT])?;
+    debug!("Start loop that intercepts signals");
+
+    thread::spawn(move || {
+        for sig in signals {
+            let signal = Signal::try_from(sig).expect("Signal is not valid");
+            debug!("Init received signal: {signal}");
+
+            match kill(Pid::from_raw(child_pid), signal) {
+                Ok(_) => {
+                    debug!("Forwarded {signal} to the child");
+                    break;
+                }
+                Err(e) => {
+                    error!("Error: {}", e);
+                }
+            }
+        }
+    });
+
+    let exit_code = reap_zombies(child_pid);
+    std::process::exit(exit_code);
 }
